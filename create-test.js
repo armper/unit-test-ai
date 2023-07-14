7a1f95f3f9eb38af2494167fac895f07f2cad448
Armando Perea <aleoperea@yahoo.com>
2023-07-14 09:30:36 -0400

    removed unit test as to test that there is no unit test. added create-test.js script to pretend to send to open ai the content of the changed file.

---
+++ create-test.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const axios = require('axios');
+
+// The first argument to the script is the path to the TypeScript file
+const tsFilePath = process.argv[2];
+
+// Read the TypeScript file
+fs.readFile(tsFilePath, 'utf8', function(err, tsCode) {
+    if (err) {
+        console.error(`Error reading file ${tsFilePath}:`, err);
+        return;
+    }
+
+    // Pretend to send the TypeScript code to the OpenAI API
+    console.log(`Sending TypeScript code to OpenAI API: ${tsCode}`);
+
+    // The API would return the unit test code
+    // For now, just log a fake response
+    const fakeApiResponse = 'describe("My Component", function() { it("should do something", function() { expect(true).toBe(true); }); });';
+
+    console.log(`Received unit test code from OpenAI API: ${fakeApiResponse}`);
+
+    // Replace .ts with .spec.ts to get the test file path
+    const specFilePath = tsFilePath.replace('.ts', '.spec.ts');
+
+    // Write the unit test code to the .spec.ts file
+    fs.writeFile(specFilePath, fakeApiResponse, function(err) {
+        if (err) {
+            console.error(`Error writing to file ${specFilePath}:`, err);
+            return;
+        }
+
+        console.log(`Successfully wrote unit test to ${specFilePath}`);
+    });
+});

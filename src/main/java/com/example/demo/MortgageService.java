7c5db5ac30aa95b4f4c9f0a9f76fbb281a58da19
Armando Perea <aleoperea@yahoo.com>
2023-09-18 15:52:23 -0400

    updated the mortgage service

--- src/main/java/com/example/demo/MortgageService.java
+++ src/main/java/com/example/demo/MortgageService.java
@@ -6,20 +6,6 @@ import org.springframework.stereotype.Service;
 public class MortgageService {
 
     /*
-     * Calculate the mortgage payment for a given principal, rate and years.
-     */
-    public double calculateMortgage(double principal, double rate, double years) {
-        double monthlyRate = rate / 100 / 12;
-        double numberOfPayments = years * 12;
-
-        double mortgage = principal
-                * (monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments))
-                / (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
-
-        return mortgage;
-    }
-
-    /*
      * Calculate the monthly payment for a given principal, rate and years.
      */
     public double calculateMonthlyPayment(double principal, double rate, double years) {
@@ -31,5 +17,16 @@ public class MortgageService {
                 / (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
 
         return monthlyPayment;
+    }
+
+    /*
+     * Calculate the rate for a given principal, monthly payment and years.
+     */
+    public double calculateRate(double principal, double monthlyPayment, double years) {
+        double numberOfPayments = years * 12;
+
+        double rate = 12 * (Math.pow(monthlyPayment, 1 / numberOfPayments) - 1);
+
+        return rate;
     }
 }

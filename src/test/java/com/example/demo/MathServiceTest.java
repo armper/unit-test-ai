c73caadcc64b67755647c913bdaed63108061622
Jenkins AI <aleoperea@yahoo.com>
2023-09-21 17:30:30 +0000

    Add or update generated unit test for feature XYZ

---
+++ src/test/java/com/example/demo/MathServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import com.example.demo.MathService;
+
+public class MathServiceTest {
+
+    @Test
+    public void testAdd() {
+        MathService mathService = new MathService();
+        assertEquals(5, mathService.add(2, 3));
+        assertEquals(-1, mathService.add(2, -3));
+        assertEquals(0, mathService.add(2, -2));
+        assertEquals(7, mathService.add(0, 7));
+    }
+}

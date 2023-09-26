62cbb23a1bd2b203be19c9b8ec78ceba7b72ecc3
Armando Perea <aleoperea@yahoo.com>
2023-09-26 08:56:14 -0400

    commit

--- src/main/java/com/example/demo/MathService.java
+++ src/main/java/com/example/demo/MathService.java
@@ -9,4 +9,12 @@ public class MathService {
         return a - b;
     }
 
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
+
 }
